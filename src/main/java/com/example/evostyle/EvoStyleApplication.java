381af98fa3ac532e4f8715ee8d7b6802ca01f648
tmdcksdl <tmdcksdl99@naver.com>
2025-04-22 10:24:06 +0900

    📝 chore: 초기 커밋

---
+++ src/main/java/com/example/evostyle/EvoStyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.evostyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoStyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/evostyle/EvoStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evostyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoStyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
