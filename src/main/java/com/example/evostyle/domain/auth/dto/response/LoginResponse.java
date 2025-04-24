ead4493152b1abb026f72417987742d580f6f43a
tmdcksdl <tmdcksdl99@naver.com>
2025-04-24 23:56:14 +0900

    📝 chore: 정적 팩토리 메서드명 변경

--- src/main/java/com/example/evostyle/domain/auth/dto/response/LoginResponse.java
+++ src/main/java/com/example/evostyle/domain/auth/dto/response/LoginResponse.java
@@ -2,7 +2,7 @@ package com.example.evostyle.domain.auth.dto.response;
 
 public record LoginResponse(String accessToken) {
 
-    public static LoginResponse of(String accessToken) {
+    public static LoginResponse from(String accessToken) {
         return new LoginResponse(accessToken);
     }
 }
