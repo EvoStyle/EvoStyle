c1f4c58cb326e3fc4aa3f28fcdc3e10877cfb7bf
tmdcksdl <tmdcksdl99@naver.com>
2025-04-24 20:30:54 +0900

    ✨ feat: 비밀번호 관련 ErrorCode 추가
    - 비밀번호 불일치 관련 예외 처리 추가

--- src/main/java/com/example/evostyle/global/exception/ErrorCode.java
+++ src/main/java/com/example/evostyle/global/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     //회원 관련
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
     INVALID_MEMBER_AUTHORITY(HttpStatus.BAD_REQUEST, "유효하지 않은 Authority 입니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "올바르지 않은 비밀번호입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용중인 이메일 입니다."),
     USER_ALREADY_DELETED(HttpStatus.CONFLICT, "이미 탈퇴한 사용자입니다."),
 
