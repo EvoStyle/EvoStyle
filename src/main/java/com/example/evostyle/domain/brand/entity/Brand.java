2b1020e1c5950da20125210983f574f6f232f929
backswan <rnrwprh3434@gmail.com>
2025-04-24 23:53:23 +0900

    :sparkles: feat: 카테고리 선택을 포함하여 브랜드 생성하는 로직 구현 (CRUD 중 C)

--- src/main/java/com/example/evostyle/domain/brand/entity/Brand.java
+++ src/main/java/com/example/evostyle/domain/brand/entity/Brand.java
@@ -1,23 +1,29 @@
 package com.example.evostyle.domain.brand.entity;
 
+import com.example.evostyle.common.entity.BaseEntity;
+import com.example.evostyle.domain.brand.brandcategory.BrandCategory;
 import com.example.evostyle.domain.member.entity.Member;
+import com.example.evostyle.global.exception.BadRequestException;
+import com.example.evostyle.global.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Table(name = "brands")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Brand {
+public class Brand extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "BIGINT")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -29,7 +35,16 @@ public class Brand {
         this.member = member;
     }
 
-    public static Brand of(String name, Member member) {
+    public static Brand of(String name, Member member, List<BrandCategory> brandCategoryList) {
+        validateBrandCategoryLimit(brandCategoryList);
         return new Brand(name, member);
+    }
+
+    private static final int BRAND_CATEGORY_LIMIT = 3;
+
+    public static void validateBrandCategoryLimit(List<BrandCategory> brandCategoryList) {
+        if (brandCategoryList.size() > BRAND_CATEGORY_LIMIT) {
+            throw new BadRequestException(ErrorCode.CATEGORY_LIMIT_EXCEEDED);
+        }
     }
 }
