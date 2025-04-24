a1abb4f03f1439e02bd789d07b7944b9309d347d
tmdcksdl <tmdcksdl99@naver.com>
2025-04-25 00:40:02 +0900

    ♻️ refactor: Repository 어노테이션 제거

--- src/main/java/com/example/evostyle/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/evostyle/domain/member/repository/MemberRepository.java
@@ -2,11 +2,9 @@ package com.example.evostyle.domain.member.repository;
 
 import com.example.evostyle.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail (String email);
