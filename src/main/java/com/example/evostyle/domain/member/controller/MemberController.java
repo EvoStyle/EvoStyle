68cf3acddb49eca84b588ae070c7e67bc7412e9e
tmdcksdl <tmdcksdl99@naver.com>
2025-04-25 11:46:52 +0900

    ♻️ refactor: 사용자 조회, 수정 로직 수정
    - 로그인한 사용자와 수정하려고 하는 사용자의 정보가 일치하는지 확인하는 로직 추가

--- src/main/java/com/example/evostyle/domain/member/controller/MemberController.java
+++ src/main/java/com/example/evostyle/domain/member/controller/MemberController.java
@@ -18,8 +18,11 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{memberId}")
-    public ResponseEntity<MemberResponse> readMember(@PathVariable(name = "memberId") Long memberId) {
-        MemberResponse memberResponse = memberService.readMember(memberId);
+    public ResponseEntity<MemberResponse> readMember(
+        @PathVariable(name = "memberId") Long memberId,
+        HttpServletRequest request
+    ) {
+        MemberResponse memberResponse = memberService.readMember(memberId, request);
 
         return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
     }
@@ -27,9 +30,10 @@ public class MemberController {
     @PatchMapping("/{memberId}")
     public ResponseEntity<MemberResponse> updateMember(
         @PathVariable(name = "memberId") Long memberId,
-        @RequestBody UpdateMemberRequest request
+        @RequestBody UpdateMemberRequest request,
+        HttpServletRequest httpServletRequest
     ) {
-        MemberResponse memberResponse = memberService.updateMember(memberId, request);
+        MemberResponse memberResponse = memberService.updateMember(memberId, request, httpServletRequest);
 
         return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
     }
--- src/main/java/com/example/evostyle/domain/member/service/MemberService.java
+++ src/main/java/com/example/evostyle/domain/member/service/MemberService.java
@@ -10,6 +10,7 @@ import com.example.evostyle.domain.member.repository.MemberRepository;
 import com.example.evostyle.global.exception.ErrorCode;
 import com.example.evostyle.global.exception.ForbiddenException;
 import com.example.evostyle.global.exception.NotFoundException;
+import com.example.evostyle.global.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,17 +24,29 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
 
-    public MemberResponse readMember(Long memberId) {
+    public MemberResponse readMember(Long memberId, HttpServletRequest request) {
+        Long loginMemberId = LoginMemberUtil.getMemberId(request, jwtUtil);
+
         Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+        if (!member.getId().equals(loginMemberId)) {
+            throw new UnauthorizedException(ErrorCode.FORBIDDEN_MEMBER_OPERATION);
+        }
 
         return MemberResponse.from(member);
     }
 
     @Transactional
-    public MemberResponse updateMember(Long memberId, UpdateMemberRequest request) {
+    public MemberResponse updateMember(Long memberId, UpdateMemberRequest request, HttpServletRequest httpServletRequest) {
+        Long loginMemberId = LoginMemberUtil.getMemberId(httpServletRequest, jwtUtil);
+
         Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+
+        if (!member.getId().equals(loginMemberId)) {
+            throw new UnauthorizedException(ErrorCode.FORBIDDEN_MEMBER_OPERATION);
+        }
 
         member.updateMember(request.nickname(), request.age(), request.phoneNumber());
 
