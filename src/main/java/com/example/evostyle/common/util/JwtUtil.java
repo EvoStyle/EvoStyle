893121e4f73947c03a529b36b9cf903fe79f739c
tmdcksdl <tmdcksdl99@naver.com>
2025-04-25 11:45:13 +0900

    📝 chore: 띄어쓰기 수정

--- src/main/java/com/example/evostyle/common/util/JwtUtil.java
+++ src/main/java/com/example/evostyle/common/util/JwtUtil.java
@@ -31,7 +31,7 @@ public class JwtUtil {
     private String secretKey;
 
     private Key key;
-    private final SignatureAlgorithm  signatureAlgorithm = SignatureAlgorithm.HS256;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     @PostConstruct
     public void init() {
