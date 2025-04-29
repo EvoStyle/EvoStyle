ed29b1a274e91a1a938d263de324fc38531495db
afkorigin <gnstldgk@naver.com>
2025-04-29 20:00:18 +0900

    :bugs: bu

--- src/main/java/com/example/evostyle/domain/delivery/entity/Delivery.java
+++ src/main/java/com/example/evostyle/domain/delivery/entity/Delivery.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "delivery")
+@Table(name = "deliverys")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Delivery {
 
