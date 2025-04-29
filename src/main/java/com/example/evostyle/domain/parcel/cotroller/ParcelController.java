3f192f50f42fbc4c130269584980162cb51d0dc9
afkorigin <gnstldgk@naver.com>
2025-04-29 21:19:23 +0900

    :sparkles: feat 경로에 api 추가

--- src/main/java/com/example/evostyle/domain/parcel/cotroller/ParcelController.java
+++ src/main/java/com/example/evostyle/domain/parcel/cotroller/ParcelController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth/parcel")
+@RequestMapping("/api/parcel")
 @RequiredArgsConstructor
 public class ParcelController {
 
