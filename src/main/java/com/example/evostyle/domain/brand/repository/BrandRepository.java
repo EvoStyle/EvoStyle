23c8530c5e68bb087d5d90fd9fb196989a1da6fb
tmdcks_99 <128008378+tmdcksdl@users.noreply.github.com>
2025-05-03 20:31:08 +0900

    🐛 fix: 사용하지 않는 메서드 삭제

--- src/main/java/com/example/evostyle/domain/brand/repository/BrandRepository.java
+++ src/main/java/com/example/evostyle/domain/brand/repository/BrandRepository.java
@@ -13,8 +13,6 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
 
     boolean existsByName(String name);
 
-    Optional<Brand> findByIdAndIsDeletedFalse(Long brandId);
-
     @Query("SELECT b.id FROM Brand b WHERE b.member.id = :memberId")
     List<Long> findBrandIdsByMemberId(@Param("memberId") Long memberId);
 
