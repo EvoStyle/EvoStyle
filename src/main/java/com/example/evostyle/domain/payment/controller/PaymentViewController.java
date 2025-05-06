bcc17bdcbc40d29776fe0212fc9633e726748081
lee sujin <jin18302@naver.com>
2025-05-07 00:13:29 +0900

    ✨ feat	: 주문 상태

--- src/main/java/com/example/evostyle/domain/payment/controller/PaymentViewController.java
+++ src/main/java/com/example/evostyle/domain/payment/controller/PaymentViewController.java
@@ -1,7 +1,5 @@
 package com.example.evostyle.domain.payment.controller;
 
-
-import com.example.evostyle.domain.order.repository.OrderRepository;
 import com.example.evostyle.domain.payment.dto.response.PaymentCheckoutResponse;
 import com.example.evostyle.domain.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("/api/payment")
@@ -25,5 +22,4 @@ public class PaymentViewController {
         model.addAttribute("orderInfo", checkoutResponse);
         return "payment/checkout";
     }
-
 }
