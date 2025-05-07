2de1351327f9546f56a3abf9b0e572f26bdd2024
tmdcksdl <tmdcksdl99@naver.com>
2025-05-07 16:15:39 +0900

    ♻️ refactor: 보기 편하게 줄바꿈 수정

--- src/main/java/com/example/evostyle/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/com/example/evostyle/domain/auth/dto/request/LoginRequest.java
@@ -4,10 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-    @Email(message = "이메일 형식이 올바르지 않습니다.")
-    @NotBlank
-    String email,
-    @NotBlank
-    String password
+    @Email(message = "이메일 형식이 올바르지 않습니다.") String email,
+    @NotBlank String password
 ) {
 }
