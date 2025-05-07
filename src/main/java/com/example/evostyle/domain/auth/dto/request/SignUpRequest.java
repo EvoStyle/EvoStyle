e16db7653b215a8f8c541121c4687807d2ce6d59
tmdcksdl <tmdcksdl99@naver.com>
2025-05-07 16:26:09 +0900

    ♻️ refactor: Valid 검증 수정

--- src/main/java/com/example/evostyle/domain/auth/dto/request/SignUpRequest.java
+++ src/main/java/com/example/evostyle/domain/auth/dto/request/SignUpRequest.java
@@ -2,17 +2,16 @@ package com.example.evostyle.domain.auth.dto.request;
 
 import com.example.evostyle.domain.member.entity.Authority;
 import com.example.evostyle.domain.member.entity.GenderType;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public record SignUpRequest(
     @Email(message = "이메일 형식이 올바르지 않습니다.") String email,
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{8,}$", message = "올바른 비밀번호가 아닙니다.") @NotBlank String password,
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{8,}$", message = "비밀번호는 소문자, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.")
+    @NotBlank String password,
     @NotBlank String nickname,
-    @NotBlank Integer age,
+    @NotNull @Positive(message = "나이는 양수여야 합니다.") Integer age,
     @NotBlank String phoneNumber,
-    @NotBlank Authority authority,
-    @NotBlank GenderType genderType
+    @NotNull Authority authority,
+    @NotNull GenderType genderType
 ) {
 }
