3436e21fc024e4107049f1d9912e749ea7c81a3a
tmdcksdl <tmdcksdl99@naver.com>
2025-05-08 10:17:40 +0900

    ♻️ refactor: Address에 QueryDSL 적용

---
+++ src/main/java/com/example/evostyle/domain/member/repository/AddressQueryRepository.java
@@ -0,0 +1,5 @@
+package com.example.evostyle.domain.member.repository;
+
+public interface AddressQueryRepository {
+    void updateAllBasecampFalse(Long memberId);
+}
--- src/main/java/com/example/evostyle/domain/member/repository/AddressRepository.java
+++ src/main/java/com/example/evostyle/domain/member/repository/AddressRepository.java
@@ -2,21 +2,15 @@ package com.example.evostyle.domain.member.repository;
 
 import com.example.evostyle.domain.member.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long>, AddressQueryRepository {
 
     Optional<Address> findByIdAndMemberId(Long Id, Long memberId);
 
     List<Address> findByMemberId(Long memberId);
-
-    @Modifying
-    @Query("UPDATE Address a SET a.isBasecamp = false WHERE a.member.id = :memberId")
-    void updateAllBasecampFalse(Long memberId);
 
     long countByMemberId(Long memberId);
 }
