7318bb759334eed92c0705b5ab88a05dbeb7dfa0
tmdcksdl <tmdcksdl99@naver.com>
2025-05-08 16:43:18 +0900

    ♻️ refactor: Review에 QueryDSL 적용

---
+++ src/main/java/com/example/evostyle/domain/review/repository/ReviewQueryRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.example.evostyle.domain.review.repository;
+
+import com.example.evostyle.domain.review.entity.QReview;
+import com.example.evostyle.domain.review.entity.Review;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewQueryRepositoryImpl implements ReviewQueryRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Review> findByProductId(Long productId) {
+        QReview review = QReview.review;
+
+        return queryFactory
+            .selectFrom(review)
+            .where(review.orderItem.productDetail.product.id.eq(productId))
+            .fetch();
+    }
+
+    @Override
+    public Optional<Review> findByIdAndMemberIdAndProductId(Long reviewId, Long memberId, Long productId) {
+        QReview review = QReview.review;
+
+        return Optional.ofNullable(
+            queryFactory
+            .selectFrom(review)
+            .where(review.id.eq(reviewId)
+                .and(review.member.id.eq(memberId))
+                .and(review.orderItem.productDetail.product.id.eq(productId)))
+            .fetchOne());
+    }
+}
