aebbccda656e92aad77f1c4cc3835b88416aa36e
afkorigin <gnstldgk@naver.com>
2025-05-08 13:54:37 +0900

    :sparkles: feat: 카프카리스너 예외 로그로 남기기

--- src/main/java/com/example/evostyle/domain/delivery/service/DeliveryKafkaListener.java
+++ src/main/java/com/example/evostyle/domain/delivery/service/DeliveryKafkaListener.java
@@ -22,35 +22,47 @@ public class DeliveryKafkaListener {
 
     @KafkaListener(id = "listener-1",topics = "delivery-event-topic", groupId = "delivery-update-group")
     public void updateDelivery(String message) {
-        DeliveryUserEvent deliveryEvent = jsonHelper.fromJson(message, DeliveryUserEvent.class);
-        switch (deliveryEvent.eventType()) {
-            case USER_UPDATE -> {
-                Optional<Long> delivery = deliveryUpdateService.updateDelivery(deliveryEvent);
-                if (delivery.isEmpty()) {
-                    return;
+        try {
+            DeliveryUserEvent deliveryEvent = jsonHelper.fromJson(message, DeliveryUserEvent.class);
+            switch (deliveryEvent.eventType()) {
+                case USER_UPDATE -> {
+                    Optional<Long> delivery = deliveryUpdateService.updateDelivery(deliveryEvent);
+                    if (delivery.isEmpty()) {
+                        return;
+                    }
+                    UserNotificationEvent success = UserNotificationEvent.success(deliveryEvent.userId(), delivery.get());
+                    String payload = jsonHelper.toJson(success);
+                    kafkaTemplate.send("user-notification-topic", deliveryEvent.userId().toString(), payload);
                 }
-                UserNotificationEvent success = UserNotificationEvent.success(deliveryEvent.userId(), delivery.get());
-                String payload = jsonHelper.toJson(success);
-                kafkaTemplate.send("user-notification-topic", deliveryEvent.userId().toString(), payload);
+                case ADMIN_UPDATE -> {
+                    AdminDeliveryResponse adminDeliveryResponse = deliveryUpdateService.changeDeliveryStatusToShipped(deliveryEvent);
+                    AdminNotificationEvent success = AdminNotificationEvent.success(adminDeliveryResponse);
+                    String payload = jsonHelper.toJson(success);
+                    kafkaTemplate.send("admin-notification-topic", success.deliveryId().toString(), payload);
+                }
             }
-            case ADMIN_UPDATE -> {
-                AdminDeliveryResponse adminDeliveryResponse = deliveryUpdateService.changeDeliveryStatusToShipped(deliveryEvent);
-               AdminNotificationEvent success = AdminNotificationEvent.success(adminDeliveryResponse);
-                String payload = jsonHelper.toJson(success);
-                kafkaTemplate.send("admin-notification-topic", success.deliveryId().toString(), payload);
-            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
-    }
+        }
 
     @KafkaListener(topics = "user-notification-topic", groupId = "user-notification-group")
     public void sendUser(String message) {
-        UserNotificationEvent userNotificationEvent = jsonHelper.fromJson(message, UserNotificationEvent.class);
-        kaKaoMessageService.sendMessage(userNotificationEvent);
+        try {
+            UserNotificationEvent userNotificationEvent = jsonHelper.fromJson(message, UserNotificationEvent.class);
+            kaKaoMessageService.sendMessage(userNotificationEvent);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
     @KafkaListener(topics = "admin-notification-topic", groupId = "admin-notification-group")
     public void sendAdmin(String message) {
-        AdminNotificationEvent adminNotificationEvent = jsonHelper.fromJson(message, AdminNotificationEvent.class);
-        slackMessageService.sendMessage(adminNotificationEvent);
+        try {
+            AdminNotificationEvent adminNotificationEvent = jsonHelper.fromJson(message, AdminNotificationEvent.class);
+            slackMessageService.sendMessage(adminNotificationEvent);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
